057b6bcdd52210b15fb6b39db7233ff3d9589181
RiskyKen <RiskyKen@users.noreply.github.com>
2014-06-29 20:31:22 +0100

    Added entity ID lib.

--- src/main/java/riskyken/utilities/common/entities/Entities.java
+++ src/main/java/riskyken/utilities/common/entities/Entities.java
@@ -2,6 +2,7 @@ package riskyken.utilities.common.entities;
 
 import cpw.mods.fml.common.registry.EntityRegistry;
 import riskyken.utilities.RiskyKensUtilities;
+import riskyken.utilities.common.lib.LibEntityIds;
 
 
 public class Entities {
@@ -9,7 +10,7 @@ public class Entities {
 	
 	public static void init()
 	{
-		EntityRegistry.registerModEntity(EntityFloodLightDespawner.class, "FloodLightDespawner", 0, RiskyKensUtilities.instance, 0, 20, false);
-		EntityRegistry.registerModEntity(EntityStarLightDespawner.class, "StarLightDespawner", 1, RiskyKensUtilities.instance, 0, 20, false);
+		EntityRegistry.registerModEntity(EntityFloodLightDespawner.class, "FloodLightDespawner", LibEntityIds.FLOOD_LIGHT_DESPAWN, RiskyKensUtilities.instance, 0, 20, false);
+		EntityRegistry.registerModEntity(EntityStarLightDespawner.class, "StarLightDespawner", LibEntityIds.STAR_LIGHT_DESPAWN, RiskyKensUtilities.instance, 0, 20, false);
 	}
 }
---
+++ src/main/java/riskyken/utilities/common/lib/LibEntityIds.java
@@ -0,0 +1,6 @@
+package riskyken.utilities.common.lib;
+
+public class LibEntityIds {
+	public static final short FLOOD_LIGHT_DESPAWN = 0;
+	public static final short STAR_LIGHT_DESPAWN = 1;
+}
