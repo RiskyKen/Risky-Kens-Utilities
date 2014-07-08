6ab5e8fc65bbf6596885c9431b55136ee7634bf7
RiskyKen <RiskyKen@users.noreply.github.com>
2014-07-09 00:58:21 +0100

    Added side only client to the GuiHelper

--- src/main/java/riskyken/utilities/client/gui/GuiHelper.java
+++ src/main/java/riskyken/utilities/client/gui/GuiHelper.java
@@ -1,8 +1,11 @@
 package riskyken.utilities.client.gui;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.StatCollector;
 
+@SideOnly(Side.CLIENT)
 public class GuiHelper {
 	public static void renderLocalizedGuiName(FontRenderer fontRenderer, int xSize, String name) {
 		String unlocalizedName = "container.inventory." + name + ".name";
